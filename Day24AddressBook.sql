6edc87ea7a03597021bda446146fd07d3daa3846
sonal <sonalkarle01@gmail.com>
2021-03-10 07:27:10 +0530

    Ability to add contact in addressBook

--- Day24AddressBook.sql
+++ Day24AddressBook.sql
@@ -17,3 +17,13 @@ PhoneNumber	    varchar(14),
 Email			varchar(50),
 
 )
+
+--UC3:Ability to add new contact
+INSERT INTO AddressBook(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email)VALUES
+('Ajinkya', 'Patil', 'Ghatkopar', 'Mumbai', 'Maharashtra', '400075', '8806184089', 'patil@gmail.com'),
+('Sachin', 'Shinde', 'Vikroli', 'Mumbai', 'Maharashtra', '400087', '8806184087', 'shinde@gmail.com'),
+('Priya', 'kale', 'Gokhalenagr', 'Pune', 'Maharashtra', '411016', '8806184085', 'kale@gmail.com'),
+('sneha', 'kulkarni', 'Bandra', 'Mumbai', 'Maharashtra', '400154', '8806184082', 'kulkarni@gmail.com'),
+('Pratiksha', 'kare', 'Kothrude', 'Pune', 'Maharashtra', '4110343', '9702420754', 'pratiksha@gmail.com');
+alter table AddressBook ADD Country varchar (10);
+update AddressBook set Country = 'India';
