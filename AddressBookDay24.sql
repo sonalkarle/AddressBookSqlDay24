6e87073373df1c643c65f83ee733f6816cd87b7e
sonal <sonalkarle01@gmail.com>
2021-03-10 07:54:34 +0530

    Ability to add contact in both type

--- AddressBookDay24.sql
+++ AddressBookDay24.sql
@@ -16,9 +16,6 @@
 
 
 
---UC11:Ability to add in both family and friend
-INSERT INTO  AddressBook (FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email , Type)VALUES
-('Sachin', 'Shinde', 'Vikroli', 'Mumbai', 'Maharashtra', '40007', '8806184087', 'shinde@gmail.com', 'FRIENDS');
 
 
 
--- Day24AddressBook.sql
+++ Day24AddressBook.sql
@@ -59,3 +59,9 @@ SELECT * FROM AddressBook;
 SELECT Type, COUNT(*) AS TYPE_COUNT FROM AddressBook GROUP BY Type;
 
 
+--UC11:Ability to add in both family and friend
+INSERT INTO  AddressBook (FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email , Type)VALUES
+('Sachin', 'Shinde', 'Vikroli', 'Mumbai', 'Maharashtra', '40007', '8806184087', 'shinde@gmail.com', 'FRIENDS');
+
+
+
